2efcc8e6f13036820c6a13b5945189bcdbfa8161
AmberQu <1037313052@qq.com>
2018-03-05 09:55:11 +0800

    AmberQu

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,122 @@
 <template>
-	<div>I am goods</div>
+	<div class="goods">
+		<div class="menu-wrapper">
+			<ul>
+				<li v-for="item in goods" class="menu-item">
+					<span class="text border-1px">
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+					</span>
+				</li>
+			</ul>
+		</div>
+		<div class="foods-wrapper">
+			<ul>
+				<li v-for="item in goods" class="food-list">
+					<h1 class="title">{{item.name}}</h1>
+					<ul>
+						<li v-for="food in item.foods" class="food-item">
+							<div class="icon">
+								<img :src="food.icon">
+							</div>
+							<div class="content">
+								<h2 class="name">{{food.name}}</h2>
+								<p class="desc">{{food.description}}</p>
+								<div class="extra">
+									<span>月售{{food.sellCount}}份</span>
+									<span>好评率{{food.rating}}%</span>
+								</div>
+								<div class="price">
+									<span>￥{{food.price}}</span>
+									<span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
-	export default {}
+	const ERR_OK=0;
+	export default {
+		props:{
+			seller:{
+				type:Object
+			}
+		},
+		data(){
+			return {
+				goods:[]
+			}
+		},
+		created(){
+		    this.$http.get('/api/goods').then(response=>{
+				response=response.body;
+				if(response.errno===ERR_OK){
+					this.goods=response.data;
+					// console.log(this.goods)
+				}
+		    }),
+		    this.classMap=['decrease','discount','special','invoice','guarantee']
+		}
+	}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-	
+
+	@import "../../common/stylus/mixin.styl"
+
+	.goods
+		display flex
+		position absolute
+		top 174px
+		bottom 46px
+		width 100%
+		overflow hidden
+		.menu-wrapper
+			flex 0 0 80px
+			width 80px
+			background #f3f5f7
+			.menu-item
+				display table
+				height 54px
+				width 56px
+				padding 0 12px
+				line-height 14px
+				.icon
+					display inline-block
+					width 12px
+					height 12px
+					vertical-align top
+					margin-right 2px
+					background-size 12px 12px
+					background-repeat no-repeat
+					&.decrease
+						bg-image('decrease_3')
+					&.discount
+						bg-image('discount_3')
+					&.guarantee
+						bg-image('guarantee_3')
+					&.invoice
+						bg-image('invoice_3')
+					&.special
+						bg-image('special_3')
+				.text
+					display table-cell
+					width 56px
+					vertical-align middle
+					border-1px(rgba(7,17,27,.1))
+					font-size 12px
+		.foods-wrapper
+			flex 1
+			.title
+				padding-left 14px
+				height 26px
+				line-height 26px
+				border-left 2px solid #d9dde1
+				font-size 12px
+				color rgb(147,153,159)
+				background #f3f5f7
 </style>
