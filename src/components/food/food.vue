e8625d6686fb6ee14c8136060b80140a792607a3
AmberQu <1037313052@qq.com>
2018-03-23 11:13:08 +0800

    update

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -17,13 +17,18 @@
 					<div class="price">
 						<span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
 					</div>
+					<div class="cartcontrol-wrapper">
+						<cartcontrol :food="food"></cartcontrol>
+					</div>
+					<transition name="fade">
+						<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
+					</transition>
 				</div>
-				<div class="cartcontrol-wrapper">
-					<cartcontrol :food="food"></cartcontrol>
+				<split v-show="food.info"></split>
+				<div class="info" v-show="food.info">
+					<h1 class="title">商品信息</h1>
+					<p class="text">{{food.info}}</p>
 				</div>
-				<transition name="fade">
-					<div @click="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
-				</transition>
 			</div>
 		</div>
 	</transition>
@@ -32,6 +37,7 @@
 	import BScroll from 'better-scroll'
 	import Vue from 'vue'
 	import cartcontrol from '@/components/cartcontrol/cartcontrol'
+	import split from '@/components/split/split'
 
 	export default{
 		props:{
@@ -69,7 +75,8 @@
 			}
 		},
 		components:{
-			cartcontrol
+			cartcontrol,
+			split
 		}
 	}
 </script>
@@ -107,6 +114,7 @@
 					font-size 20px
 					color #fff
 		.content
+			position relative
 			padding 18px
 			.title
 				line-height 14px
@@ -135,21 +143,39 @@
 					text-decoration line-through
 					font-size 10px
 					color rbg(147,153,159)
-		.cartcontrol-wrapper
-			position absolute
-			right 12px
-			bottom 12px
-		.buy
-			position absolute
-			right 18px
-			bottom 18px
-			z-index 10
-			height 24px
-			line-height 24px
-			padding 0 12px
-			box-sizing border-box
-			border-radius 12px
-			font-size 10px
-			color #fff
-			background rgb(0,160,220)
+			.cartcontrol-wrapper
+				position absolute
+				right 12px
+				bottom 12px
+			.buy
+				position absolute
+				right 18px
+				bottom 18px
+				z-index 10
+				height 24px
+				line-height 24px
+				padding 0 12px
+				box-sizing border-box
+				border-radius 12px
+				font-size 10px
+				color #fff
+				background rgb(0,160,220)
+				&.fade-enter-active,&.fade-leave-active
+					transition all .2s
+				&.fade-enter-to,&.fade-leave
+					opacity 1
+				&.fade-enter,&.fade-leave-to
+					opacity 0
+		.info
+			padding 18px
+			.title
+				line-height 14px
+				margin-bottom 6px
+				font-size 14px
+				color rgb(7,17,27)
+			.text
+				line-height 24px
+				padding 0 8px
+				font-size 12px
+				color rgb(77,85,93)
 </style>
